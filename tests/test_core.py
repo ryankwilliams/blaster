5086a3fcc98421b1b6ebf92998b9bcfce576bb48
Ryan Williams <rwilliams5262@gmail.com>
2017-08-22 20:06:52 -0400

    Unit tests to cover engine.processor module
    Includes updated doc strings for all unit tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -12,11 +12,18 @@ from blaster.core import BlasterError, CalcTimeMixin, LoggerMixin, \
 
 
 class TestBlasterError(object):
-    """Test blaster error class."""
+    """Unit tests to cover blaster error.
+
+    Class provides tests to gain better code coverage.
+    """
 
     @staticmethod
     def test_blaster_error():
-        """Test blaster error."""
+        """Raise a blaster error.
+
+        This method tests raising the blaster error class. It will also
+        verify the values returned from the exception.
+        """
         try:
             raise BlasterError('Blaster error!')
         except BlasterError as ex:
@@ -25,43 +32,72 @@ class TestBlasterError(object):
 
 
 class TestLoggerMixin(object):
-    """Test logger mixin class."""
+    """Unit tests to cover blaster logger mixin class.
+
+    Class provides both positive and negative tests to gain better code
+    coverage.
+    """
 
     @staticmethod
     def test_create_logger():
-        """Test create blaster logger."""
+        """Create a logger mixin object.
+
+        This method tests creating a logger mixin object from the logger mixin
+        class. Once the object is created, it will verify it is an instance of
+        the logger mixin class.
+        """
         logger = LoggerMixin()
         logger.create_blaster_logger('info')
         assert_is_instance(logger, LoggerMixin)
 
     @staticmethod
     def test_logger_object():
-        """Test blaster logger object."""
+        """Log a message with a logger mixin object.
+
+        This method tests logging a message with a logger mixin object.
+        """
         logger = LoggerMixin()
         logger.create_blaster_logger('info')
         logger.logger.info('Blaster!')
 
 
 class TestCalcTimeMixin(object):
-    """Test calculate time mixin class."""
+    """Unit tests to cover blaster calculate time mixin class.
+
+    Class provides both positive and negative tests to gain better code
+    coverage.
+    """
 
     @staticmethod
     def test_save_start_time():
-        """Test saving start time."""
+        """Save start time.
+
+        This method tests saving the start time. It will verify that the value
+        saved is not none.
+        """
         calc = CalcTimeMixin()
         calc.start()
         assert_is_not_none(calc._start_time)
 
     @staticmethod
     def test_save_end_time():
-        """Test saving end time."""
+        """Save end time.
+
+        This method tests saving the end time. It will verify that the value
+        saved is not none.
+        """
         calc = CalcTimeMixin()
         calc.end()
         assert_is_not_none(calc._end_time)
 
     @staticmethod
     def test_time_delta():
-        """Test time delta between two endpoints."""
+        """Calculate delta between two time endpoints.
+
+        This method tests calculating the time delta between two endpoints
+        (start and end). It will verify the returned values are the correct
+        data type.
+        """
         calc = CalcTimeMixin()
         calc.start()
         sleep(1)
@@ -73,18 +109,31 @@ class TestCalcTimeMixin(object):
 
 
 class TestTaskDefinition(object):
-    """Test task definition class."""
+    """Unit tests to cover blaster task definition class.
+
+    Class provides both positive and negative tests to gain better code
+    coverage.
+    """
 
     @staticmethod
     def create_task():
-        """Test create a task."""
+        """Create a task.
+
+        This method tests creating a task. It will verify the task is an
+        instance of the task definition class. As well as verify it has the
+        name attribute.
+        """
         task_def = TaskDefinition(name='blaster')
         assert_is_instance(task_def, TaskDefinition)
         assert_true(hasattr(task_def, 'name'))
 
     @staticmethod
     def test_valid_task_definition():
-        """Test whether a task definition is valid."""
+        """Checks if a task definition is valid.
+
+        This method tests if a task definition is valid by calling the
+        is_valid method that each task has.
+        """
         task_def = TaskDefinition(
             dict(
                 name='test',
@@ -96,7 +145,11 @@ class TestTaskDefinition(object):
 
     @staticmethod
     def test_invalid_task_definition():
-        """Test whether a task definition is invalid."""
+        """Check if a task definition is invalid.
+
+        This method tests if a task definition is invalid by calling the
+        is_valid method that each task has.
+        """
         task_def = TaskDefinition(
             dict(
                 name='test',
@@ -107,31 +160,47 @@ class TestTaskDefinition(object):
 
 
 class TestResultsList(object):
-    """Test results list class."""
+    """Unit tests to cover blaster results list class."""
 
     @staticmethod
     def create_results():
-        """Test create a results list."""
+        """Create blaster results.
+
+        This method tests creating a blaster results list. It will verify
+        the object is an instance of the results list class.
+        """
         res = ResultsList()
         assert_is_instance(res, ResultsList)
 
     @staticmethod
     def test_analyze_pass_results():
-        """Test analyzing passed results."""
+        """Analyze passed blaster results.
+
+        This method tests the results list object to see if the results are
+        valid. (positive test)
+        """
         res = ResultsList()
         res.append(dict(name='item1', status=0))
         assert_equal(res.analyze(), 0)
 
     @staticmethod
     def test_analyze_failed_results():
-        """Test analyzing failed results."""
+        """Analyze failed blaster results.
+
+        This method tests the results list object to see if the results are
+        invalid. (negative test)
+        """
         res = ResultsList()
         res.append(dict(name='item1', status=1))
         assert_equal(res.analyze(), 1)
 
     @staticmethod
     def test_coordinate_valid_results():
-        """Test coordinate valid results."""
+        """Coordinate passed blaster results.
+
+        This method tests coordinating task data to the results data.
+        (positive test)
+        """
         res = ResultsList()
         res.append(dict(name='item1', bid='1234', status=1))
         task = dict(name='item1', bid='1234')
@@ -140,7 +209,11 @@ class TestResultsList(object):
     @staticmethod
     @raises(KeyError)
     def test_coordinate_invalid_results():
-        """Test coordinate invalid results."""
+        """Coordinate failed blaster results.
+
+        This method tests coordinating task data to the results data.
+        (negative test)
+        """
         res = ResultsList()
         res.append(dict(name='item1', bid='1234', status=1))
         task = dict(name='item1')
